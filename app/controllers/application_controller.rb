895b51f475153b67e158076785f9dc9e7a9e1463
Adao Natalino <adaojosen@hotmail.com>
2020-07-09 18:03:08 +0100

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,6 @@ class ApplicationController < ActionController::API
     end
    
     def authorized
-        # byebug
       render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
     end
   end
