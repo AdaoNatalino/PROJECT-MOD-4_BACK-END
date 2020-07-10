f7a454997a6e0942e4a75bec674f55e30ae5e5c3
Adao Natalino <adaojosen@hotmail.com>
2020-07-10 10:50:49 +0100

    heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,19 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://beer-shop-london.netlify.app/'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+  
+  allow do
+    origins 'http://localhost:3000/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
 end
